9ff11b769182a23cb9dce688f0c23b7e1da7aaa1
Daniel Rothblatt <djrothblatt@gmail.com>
2017-05-04 16:46:45 -0400

    Create bomb class

---
+++ app/bomb.js
@@ -0,0 +1,19 @@
+export default class Bomb {
+    constructor({x, y}) {
+	this.x = x;
+	this.y = y;
+	this.range = 3;
+	this.lifetime = 3; //seconds
+    }
+
+    explosionPath() {
+	function ring(i) {
+	    return [{x: this.x - i, y: this.y},
+		    {x: this.x + i, y: this.y},
+		    {x: this.x, y: this.y - i},
+		    {x: this.x, y: this.y + i}];
+	}
+	ring = ring.bind(this);
+	return [1, 2, 3].reduce((acc, i) => acc.concat(ring(i)), [{x: this.x, y: this.y}]);
+    }
+}
